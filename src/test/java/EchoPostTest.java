eb27252863250e3f6bd8b876205c6682edf89d80
Nata test <loginatom@gmail.com>
2025-10-15 21:25:24 +0500

    Initial commit

---
+++ src/test/java/EchoPostTest.java
@@ -0,0 +1,40 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class EchoPostTest {
+
+    @Test
+    void postShouldReturnSentData() {
+        String sentData = "some data";
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body(sentData)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                // Здесь JSON-путь обращается к полю "data", которое содержит отправленное тело
+                .body("data", equalTo(sentData));
+    }
+
+    @Test
+    void postShouldFailWithWrongBody() {
+        String sentData = "some data";
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body(sentData)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                // Преднамеренно неверная проверка - для демонстрации падения теста
+                .body("data", equalTo("wrong data"));
+    }
+}
